3e2e4b21c19857e1b414945ac1c7011c0cdcc1e6
Xavier Xiqués <xavier.xiques@gmail.com>
2025-03-15 13:05:47 +0100

    Add id and static method to create reservation

--- src/main/kotlin/com/reservation/api/domain/reservations/Reservation.kt
+++ src/main/kotlin/com/reservation/api/domain/reservations/Reservation.kt
@@ -1,12 +1,27 @@
 package com.reservation.api.domain.reservations
 
 import java.time.LocalDateTime
+import java.util.*
 
 data class Reservation(
+    val id: String,
     val date: LocalDateTime,
     val customerDetails: CustomerDetails,
     val partySize: Int
-)
+) {
+    companion object {
+        fun create(
+            date: LocalDateTime,
+            customerDetails: CustomerDetails,
+            partySize: Int
+        ) = Reservation(
+            id = UUID.randomUUID().toString(),
+            date = date,
+            customerDetails = customerDetails,
+            partySize = partySize
+        )
+    }
+}
 
 data class CustomerDetails(
     val name: String,
