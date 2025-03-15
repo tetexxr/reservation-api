3ce3e8c79770aca5559a21632a82346d1de5f5da
Xavier Xiqués <xavier.xiques@gmail.com>
2025-03-15 19:14:28 +0100

    Table suitable fully tested

---
+++ src/test/kotlin/com/reservation/api/domain/tables/TableShould.kt
@@ -0,0 +1,38 @@
+package com.reservation.api.domain.tables
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class TableShould {
+
+    @Test
+    fun `should return true when party size is even and matches maximum seating capacity`() {
+        val partySize = 4
+        assertThat(table.isSuitableForPartySize(partySize)).isTrue()
+    }
+
+    @Test
+    fun `should return true when party size is odd and one less than maximum seating capacity`() {
+        val partySize = 3
+        assertThat(table.isSuitableForPartySize(partySize)).isTrue()
+    }
+
+    @Test
+    fun `should return false when party size is even and does not match maximum seating capacity`() {
+        val partySize = 6
+        assertThat(table.isSuitableForPartySize(partySize)).isFalse()
+    }
+
+    @Test
+    fun `should return false when party size is odd and not one less than maximum seating capacity`() {
+        val partySize = 1
+        assertThat(table.isSuitableForPartySize(partySize)).isFalse()
+    }
+
+    companion object {
+        private val table = Table(
+            number = TableNumber(1),
+            maximumSeatingCapacity = 4
+        )
+    }
+}
