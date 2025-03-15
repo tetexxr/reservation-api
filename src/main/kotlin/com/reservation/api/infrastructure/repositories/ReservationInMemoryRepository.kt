7d954d19d0af3a28fc5a6ffd22ff9ae456719390
Xavier Xiqués <xavier.xiques@gmail.com>
2025-03-15 14:11:54 +0100

    Implementation

--- src/main/kotlin/com/reservation/api/infrastructure/repositories/ReservationInMemoryRepository.kt
+++ src/main/kotlin/com/reservation/api/infrastructure/repositories/ReservationInMemoryRepository.kt
@@ -5,7 +5,7 @@ import com.reservation.api.domain.reservations.ReservationId
 import com.reservation.api.domain.reservations.ReservationRepository
 
 class ReservationInMemoryRepository : ReservationRepository {
-    
+
     override fun insert(reservation: Reservation): Reservation {
         reservations.add(reservation)
         return reservation
@@ -13,6 +13,11 @@ class ReservationInMemoryRepository : ReservationRepository {
 
     override fun findById(reservationId: ReservationId): Reservation? {
         return reservations.find { it.id == reservationId }
+    }
+
+    override fun update(reservation: Reservation) {
+        val index = reservations.indexOfFirst { it.id == reservation.id }
+        reservations[index] = reservation
     }
 
     companion object {
