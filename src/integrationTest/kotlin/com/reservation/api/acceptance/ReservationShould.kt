5ddd3f5b0874c2cee8d27bab2205978e531b59fc
Xavier Xiqués <xavier.xiques@gmail.com>
2025-03-15 12:34:47 +0100

    Very basic integration test to create reservation

---
+++ src/integrationTest/kotlin/com/reservation/api/acceptance/ReservationShould.kt
@@ -0,0 +1,41 @@
+package com.reservation.api.acceptance
+
+import com.reservation.api.Application
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.http.MediaType
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+
+@SpringBootTest(classes = [Application::class])
+@ActiveProfiles("integration-test")
+@AutoConfigureMockMvc
+class ReservationShould {
+
+    @Autowired
+    private lateinit var mvc: MockMvc
+
+    @Test
+    fun `create a reservation`() {
+        mvc.perform(
+            post("/v1/reservations")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(
+                    """
+                    {
+                        "date": "2021-10-10T10:00:00",
+                        "name": "John",
+                        "email": "john@test.com",
+                        "phoneNumber": "931111111",
+                        "partySize": 4
+                    }
+                    """
+                )
+        ).andExpect(status().isCreated())
+    }
+
+}
