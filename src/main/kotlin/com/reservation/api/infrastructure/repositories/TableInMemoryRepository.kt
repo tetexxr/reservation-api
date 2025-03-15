f8b72eccff2b35b16cd2217882d8b451dfc71fa6
Xavier Xiqués <xavier.xiques@gmail.com>
2025-03-15 19:14:56 +0100

    Define table number value object

--- src/main/kotlin/com/reservation/api/infrastructure/repositories/TableInMemoryRepository.kt
+++ src/main/kotlin/com/reservation/api/infrastructure/repositories/TableInMemoryRepository.kt
@@ -1,7 +1,8 @@
 package com.reservation.api.infrastructure.repositories
 
-import com.reservation.api.domain.availability.Table
-import com.reservation.api.domain.availability.TableRepository
+import com.reservation.api.domain.tables.Table
+import com.reservation.api.domain.tables.TableNumber
+import com.reservation.api.domain.tables.TableRepository
 
 class TableInMemoryRepository : TableRepository {
 
@@ -11,14 +12,14 @@ class TableInMemoryRepository : TableRepository {
 
     companion object {
         private val tables = listOf(
-            Table(number = 1, maximumSeatingCapacity = 2),
-            Table(number = 2, maximumSeatingCapacity = 2),
-            Table(number = 3, maximumSeatingCapacity = 4),
-            Table(number = 4, maximumSeatingCapacity = 4),
-            Table(number = 5, maximumSeatingCapacity = 6),
-            Table(number = 6, maximumSeatingCapacity = 6),
-            Table(number = 7, maximumSeatingCapacity = 8),
-            Table(number = 8, maximumSeatingCapacity = 10)
+            Table(number = TableNumber(1), maximumSeatingCapacity = 2),
+            Table(number = TableNumber(2), maximumSeatingCapacity = 2),
+            Table(number = TableNumber(3), maximumSeatingCapacity = 4),
+            Table(number = TableNumber(4), maximumSeatingCapacity = 4),
+            Table(number = TableNumber(5), maximumSeatingCapacity = 6),
+            Table(number = TableNumber(6), maximumSeatingCapacity = 6),
+            Table(number = TableNumber(7), maximumSeatingCapacity = 8),
+            Table(number = TableNumber(8), maximumSeatingCapacity = 10)
         )
     }
 }
