c8c1e7dd9d2bfc5fe8b637fd542ce2a13554d3d7
Xavier Xiqués <xavier.xiques@gmail.com>
2025-03-15 19:07:12 +0100

    Remove not needed file

--- src/main/kotlin/com/reservation/api/domain/availability/AvailabilityRepository.kt
+++
@@ -1,7 +0,0 @@
-package com.reservation.api.domain.availability
-
-import java.time.LocalDateTime
-
-interface AvailabilityRepository {
-    fun check(reservationTime: LocalDateTime, partySize: Int): List<Table>
-}
